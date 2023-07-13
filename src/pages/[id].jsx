6c5f3b8c55c0f00d1ed988d1103d4083b19e973f
LordCookis <azat525210@gmail.com>
2023-07-13 23:25:35 +0600

    New code

--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -2,7 +2,26 @@ import { useEffect, useState } from "react"
 import { useRouter } from 'next/router'
 
 export default function test() {
-  const [test, setTest] = useState({})
+  const [test, setTest] = useState({
+    name: "",
+    type: true,
+    owner: "",
+    questions: [{
+      id: 0,
+      textQ: "",
+      answers: [{
+        id: 0,
+        textA: "",
+        state: false
+      },
+      {
+        id: 0,
+        textA: "",
+        state: false
+      }]
+    }]
+  })
+  const [checkAnswer, setCheckAnswer] = useState([])
   const [numQ, setNumQ] = useState(0)
   const router = useRouter()
   const { id } = router.query
@@ -17,24 +36,45 @@ export default function test() {
     })
     const data = await result.json()
     setTest(data)
-    //console.log(data)
+    setCheckAnswer(Array(data.questions.length).fill(null))
+  }
+
+  const selectAnswer = (id) => {
+    const result = [...checkAnswer]
+    const trueAnswer = result[numQ] || []
+    const answerId = trueAnswer.indexOf(id)
+    if (answerId >= 0) {
+      trueAnswer.splice(answerId, 1)
+    } else {
+      trueAnswer.push(id)
+    }
+    result[numQ] = trueAnswer
+    setCheckAnswer(result)
+  }
+
+  const backQ = () => {
+    numQ ? setNumQ(numQ - 1) : setNumQ(test.questions.length - 1)
+  }
+
+  const nextQ = () => {
+    test.questions.length !== numQ + 1 ? setNumQ(numQ + 1) : setNumQ(0)
   }
 
   return(
     <div className="testDivPage">
       <span className="testName">{test.name}</span>
-      <span className="testQuestion">{/*test.questions[numQ].textQ*/console.log(test.questions)}</span>
+      <span className="testQuestion">{test.questions[numQ].textQ}</span>
       <div className="answerDiv">
         {test.questions[numQ].answers.map((answer) => (
-        <button className="testAnswer">{answer.textA}</button>
+        <button className={`${checkAnswer[numQ]?.includes(answer.id) ? `trueAnswer` : `testAnswer`}`} onClick={()=>selectAnswer(answer.id)}>{answer.textA}</button>
         ))}
       </div>
       <div className="divPage">
-        <button className="buttonPage">НАЗАД</button>
+        <button className="buttonPage" onClick={backQ}>НАЗАД</button>
         <button className="buttonExit">X</button>
-        <button className="buttonPage">ДАЛЕЕ</button>
+        <button className="buttonPage" onClick={nextQ}>ДАЛЕЕ</button>
       </div>
-      <span className="span">1 / 5</span>
+      <span className="span">{numQ + 1} / {test.questions.length}</span>
       <button className="buttonEnd">ЗАВЕРШИТЬ</button>
     </div>
   )
